cb8449a8c1e291c2264287d3059179bf6d29ea62
22wllutz <151688318+22wllutz@users.noreply.github.com>
2023-12-09 01:01:29 -0500

    Create DataOptions.java

---
+++ DataOptions.java
@@ -0,0 +1,10 @@
+public class DataOptions {
+   public String open;
+   public String high;
+   public String low;
+   public String close;
+   public String abjustedClose;
+   public String volume;
+   public String dividendAmount;
+}
+   
